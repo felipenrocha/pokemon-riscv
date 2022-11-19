82fabedfdfd49975acdabdba6758da0a729796f9
felipenrocha19 <felipenrocha19@gmail.com>
2022-11-19 12:35:12 -0300

    first upgrades wip: clean lat used tile, remove background color from character

--- jogo.s
+++ jogo.s
@@ -1,27 +1,93 @@
+.data 
+CHAR_POS: .half 0,0
+OLD_CHAR_POS:	.half 0,0			# x, y
 .text
-
 SETUP:
-	call PRINT_MAP
-	call PRINT_USER
-#
-PRINT_MAP:
 	la a0, home_1f
 	li a1, 0
 	li a2, 0
 	li a3, 0
 	call PRINT
-	
-	
-	
-PRINT_USER:
-	la a0, hero_1
-	li a1, 160
-	li a2, 120
-	li a3, 0 
+	li a3, 1
 	call PRINT
 	
+#
+
+GAME_LOOP:
+
+	call KEY2
+	xori s0, s0, 1
+	
+	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
+	
+	
+	la t0, CHAR_POS
+	la a0, hero_2
+	lh a1, 0(t0)
+	lh a2, 2(t0)
+	mv a3, s0
+	call PRINT
+
+	li t0,0xFF200604
+	sw s0, 0(t0)
+	
+	j GAME_LOOP
 
 
+
+	
+
+KEY2:		li t1,0xFF200000		# carrega o endereco de controle do KDMMIO
+		lw t0,0(t1)			# Le bit de Controle Teclado
+		andi t0,t0,0x0001		# mascara o bit menos significativo
+   		beq t0,zero,FIM   	   	# Se nao ha tecla pressionada entao vai para FIM
+  		lw t2,4(t1)  			# le o valor da tecla tecla
+		
+		li t0,'w'
+		beq t2,t0,CHAR_CIMA		# se tecla pressionada for 'w', chama CHAR_CIMA
+		
+		li t0,'a'
+		beq t2,t0,CHAR_ESQ		# se tecla pressionada for 'a', chama CHAR_CIMA
+		
+		li t0,'s'
+		beq t2,t0,CHAR_BAIXO		# se tecla pressionada for 's', chama CHAR_CIMA
+		
+		li t0,'d'
+		beq t2,t0,CHAR_DIR		# se tecla pressionada for 'd', chama CHAR_CIMA
+	
+FIM:		ret				# retorna
+				
+
+				
+CHAR_ESQ:
+	la t0, 	CHAR_POS
+	lh t1, 0(t0)
+	addi t1, t1, -16
+	sh t1, 0(t0)
+	ret
+	
+CHAR_DIR:
+	la t0, 	CHAR_POS
+	lh t1, 0(t0)
+	addi t1, t1, 16
+	sh t1, 0(t0)
+	ret
+	
+																																											
+CHAR_CIMA:
+	la t0,CHAR_POS
+	lh t1,2(t0)			# carrega o y atual do personagem
+	addi t1,t1,-16			# decrementa 16 pixeis
+	sh t1,2(t0)			# salva
+	ret
+	
+					
+CHAR_BAIXO:
+	la t0,CHAR_POS
+	lh t1,2(t0)			# carrega o y atual do personagem
+	addi t1,t1,16			# incrementa 16 pixeis
+	sh t1,2(t0)			# salva
+	ret												
 
 #	a0 = ENDERECO DA IMAGEM
 #	a1 = x
@@ -35,6 +101,11 @@ PRINT_USER:
 #	t3 = contador de coluna
 #	t4 = altura
 #	t5 = largura
+
+
+
+
+
 
 
 
@@ -78,7 +149,10 @@ PRINT_LINHA:
 	blt t2, t4, PRINT_LINHA
 	
 	ret
-
+	
+	
+	
 .data
 .include "sprites/backgrounds/home_1f.data"
 .include "sprites/characters/hero_1.data"
+.include "sprites/characters/hero_2.data"
